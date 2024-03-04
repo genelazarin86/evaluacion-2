c4829d0d6fd4b03f8752557b9e38638a2aaaa989
gene lazarin <159508631+genelazarin86@users.noreply.github.com>
2024-03-04 16:59:10 -0600

    Add files via upload

---
+++ practicas/Prueba.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class prueba{
+
+public static void main(String args[]){
+Scanner captura= Scanner (System.in);
+System.out.println("introduce tu nombre:");
+String nombre = captura.nextLine();
+System.out.println(nombre);
+}
+
+
+
+
+}
