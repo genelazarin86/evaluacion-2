83dbfe53434abf7aa4886f40e34ee2086a8d6fa1
gene ricardo lazarin nuñez <159508631+genelazarin86@users.noreply.github.com>
2024-04-09 16:53:25 -0600

    Add files via upload

---
+++ eva2_18_FOR/src/eva2_18_for/Eva2_18_FOR.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_18_for;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva2_18_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i = 1; i <=10; i++) {
+            System.out.print(i+"-");
+        }
+        System.out.println("");
+            for(int i = 10; i >=1; i--) {
+            System.out.print(i+"-");
+            
+       
+            
+        }
+            
+    
+    for(int i = 0; i <=100; i+=2){
+    
+            System.out.println(i +"-");
+}
+    }
+}
