be9567c786a5f80fbe3dfd93db397285062b39ee
gene ricardo lazarin nuñez <159508631+genelazarin86@users.noreply.github.com>
2024-04-15 15:49:34 -0700

    Add files via upload

---
+++ EVA2_24_ADIVINAR_WHILE/src/eva2_24_adivinar_while/EVA2_24_ADIVINAR_WHILE.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_24_adivinar_while;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_24_ADIVINAR_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
